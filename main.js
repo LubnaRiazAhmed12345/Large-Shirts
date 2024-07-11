20f12cd81d80f01fd09a66f8cbaa2635813d41f5
Lubna <muspaksamsu@gmail.com>
2024-07-12 01:41:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function make_shirt(size, printMessage) {
+    if (size === void 0) { size = "Large"; }
+    if (printMessage === void 0) { printMessage = " I love typescript"; }
+    console.log("creating a $(size) shirt with the ".concat(printMessage, " prints on shirt"));
+}
+make_shirt();
+make_shirt("Medium");
+make_shirt("small", "i love javascript");
